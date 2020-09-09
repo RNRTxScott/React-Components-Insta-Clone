ac5b5bf95e7dd3154d6b0b36d11b5d889b86f31a
Scott Vanderburg <s.vanderburg@realnerdsrt.com>
2020-09-09 11:20:06 -0600

    something

--- src/App.js
+++ src/App.js
@@ -12,23 +12,7 @@
 // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
 
 // To make the search bar work (which is stretch) we'd need another state to hold the search term.
-
-
-import React, {useState} from 'react';
-import Posts from './components/Posts';
-import SearchBar from './components/SearchBar';
-import dummyData from './dummy-data';
-
-
-import './App.css';
-
-const App = () => {
-  const [posts, setPosts] = useState(dummyData)
-  
-  
-
-  const likePost = postId => {
-    /*
+/*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
       The state of the app lives at the top of the React tree, but it wouldn't be fair for nested components not to be able to change state!
@@ -39,14 +23,45 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
-  };
+
+import React, {useState} from 'react';
+import Posts from './components/Posts';
+import SearchBar from './components/SearchBar';
+import dummyData from './dummy-data';
+import Comments from './components/Comments';
+
+
+import './App.css';
+
+const App = () => {
+  const [posts, setPosts] = useState(dummyData)
+  
+  
+
+  /*
+   const likePost = postId => {
+    setPosts(
+      posts.map((it) => {
+        if (postID === it.id) {
+        s  return { ...it, likes: it.likes + 1};
+        };
+        return it; 
+      })
+    );
+};
+    
+    
+    
+  */
 
   return (
     <div className='App'>
-      {/* Add SearchBar and Posts here to render them */}
-      {/* Check the implementation of each component, to see what props they require, if any! */}
+      {SearchBar}
+      {Posts}
+      <Comments id="22"/>
     </div>
   );
 };
 
 export default App;
+ 
